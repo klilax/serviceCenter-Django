959f73bd02587a663fc90f9f3210b439cd2b0de1
Khalil Ahmed <klilax@hotmail.co.uk>
2022-02-24 20:15:51 +0300

    project added

--- serviceCenter/serviceCenter/urls.py
+++ serviceCenter/serviceCenter/urls.py
@@ -1,7 +1,7 @@
-"""serviceCenter URL Configuration
+"""hypercar URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import RedirectView
+from tickets.views import WelcomeView, MenuView, change_oil, inflate_tires, diagnostic, get_queue
+from tickets.views import ProcessNext
+from tickets.views import GetNext
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('welcome/', WelcomeView.as_view()),
+    path('menu/', MenuView.as_view()),
+    path('get_ticket/change_oil', change_oil),
+    path('get_ticket/inflate_tires', inflate_tires),
+    path('get_ticket/diagnostic', diagnostic),
+    path('processing/', ProcessNext.as_view()),
+    # path('/processing', ProcessNext.as_view()),
+    path('processing', ProcessNext.as_view(), name='processing'),
+    path('next/', GetNext.as_view()),
+    # re_path("/processing/?", RedirectView.as_view(url='/processing')),
+    # path('/processing/', RedirectView.as_view(url='/processing')),
 ]
--- serviceCenter/tickets/views.py
+++ serviceCenter/tickets/views.py
@@ -1,3 +1,109 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+class WelcomeView(View):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+        # return HttpResponse('')
+
+
+class MenuView(View):
+    template_name = 'menu.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+counter = 1
+line_of_cars = {'change_oil': [], 'inflate_tires': [], 'diagnostic': []}
+next_ticket = -1
+
+
+def get_ticket():
+    global counter
+    t_no = counter
+    counter += 1
+    return t_no
+
+
+def oil_time():
+    return len(line_of_cars['change_oil']) * 2
+
+
+def tire_time():
+    return oil_time() + (len(line_of_cars['inflate_tires']) * 5)
+
+
+def diagnostic_time():
+    return tire_time() + (len(line_of_cars['diagnostic']) * 30)
+
+
+def change_oil(request):
+    t_no = get_ticket()
+    w_time = oil_time()
+    line_of_cars['change_oil'].append(t_no)
+    ticket = {'no': t_no, 'waiting_time': w_time}
+
+    return render(request, 'ticket.html', {'ticket': ticket})
+
+
+def inflate_tires(request):
+    t_no = get_ticket()
+    w_time = tire_time()
+    line_of_cars['inflate_tires'].append(t_no)
+    ticket = {'no': t_no, 'waiting_time': w_time}
+
+    return render(request, 'ticket.html', {'ticket': ticket})
+
+
+def diagnostic(request):
+    t_no = get_ticket()
+    w_time = diagnostic_time()
+    line_of_cars['diagnostic'].append(t_no)
+    ticket = {'no': t_no, 'waiting_time': w_time}
+
+    return render(request, 'ticket.html', {'ticket': ticket})
+
+
+def get_queue(request):
+    ticket_queue = {
+        'change_oil': len(line_of_cars['change_oil']),
+        'inflate_tires': len(line_of_cars['inflate_tires']),
+        'diagnostic': len(line_of_cars['diagnostic']),
+    }
+    return render(request, 'processing.html', {'ticket_queue': ticket_queue})
+
+
+class ProcessNext(View):
+
+    def post(self, request, *args, **kwargs):
+        global next_ticket
+
+        if line_of_cars['change_oil']:
+            next_ticket = line_of_cars['change_oil'].pop(0)
+        elif line_of_cars['inflate_tires']:
+            next_ticket = line_of_cars['inflate_tires'].pop(0)
+        elif line_of_cars['diagnostic']:
+            next_ticket = line_of_cars['diagnostic'].pop(0)
+        else:
+            next_ticket = -1
+
+        return redirect('/processing/')
+
+    def get(self, request, *args, **kwargs):
+        ticket_queue = {
+            'change_oil': len(line_of_cars['change_oil']),
+            'inflate_tires': len(line_of_cars['inflate_tires']),
+            'diagnostic': len(line_of_cars['diagnostic']),
+        }
+        return render(request, 'processing.html', {'ticket_queue': ticket_queue})
+
+
+class GetNext(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'next.html', {'next_ticket': next_ticket})
